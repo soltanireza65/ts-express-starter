588236e0fa706bc0ba52375c7fe4ca88f939947b
Reza Soltani <soltanireza65@gmail.com>
2022-03-25 22:13:21 +0430

    ..

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -10,4 +10,24 @@ export const storage = multer.diskStorage({
   },
 });
 
-export const photoUpload = multer({ storage: storage }).single('photo');
+const checkFileType = (file, cb) => {
+  const fileTypes = /jpeg|jpg|png|gif/;
+
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    cb('Error image only');
+  }
+};
+export const photoUpload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single('photo');
